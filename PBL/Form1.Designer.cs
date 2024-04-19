ab58919229ea614daca781a118f422d503d34167
Khánh Mai Văn <mvkhanh100204@gmail.com>
2024-04-19 10:25:11 +0700

    Commit 2

--- PBL/Form1.Designer.cs
+++ PBL/Form1.Designer.cs
@@ -36,9 +36,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(335, 90);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(84, 25);
+            this.label1.Size = new System.Drawing.Size(92, 25);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Tai ngu";
+            this.label1.Text = "Huy ngu";
             // 
             // Form1
             // 
